12c3947908666b6de2cfc486f0079d30e4f7e026
ummyeahumm <wtfuknowbtw@gmail.com>
2026-06-10 16:51:24 +1200

    Update Report.js

--- models/Report.js
+++ models/Report.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const reportSchema = new mongoose.Schema({
-  // Add fields here
 }, { timestamps: true });
 
 module.exports = mongoose.model('Report', reportSchema);
