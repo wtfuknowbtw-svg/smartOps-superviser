c5b9c507d0438b117c2fb9a7e8aa9615758f0a47
ummyeahumm <wtfuknowbtw@gmail.com>
2026-06-10 16:52:53 +1200

    Update Attendance.js

--- models/Attendance.js
+++ models/Attendance.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const attendanceSchema = new mongoose.Schema({
-  // Add fields here
+  
 }, { timestamps: true });
 
 module.exports = mongoose.model('Attendance', attendanceSchema);
