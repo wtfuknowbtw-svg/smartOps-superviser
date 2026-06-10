5797141a648d51ab84cf83d7e299cdb2f10c27ee
ummyeahumm <wtfuknowbtw@gmail.com>
2026-06-10 16:52:42 +1200

    Update Escalation.js

--- models/Escalation.js
+++ models/Escalation.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const escalationSchema = new mongoose.Schema({
-  // Add fields here
+  
 }, { timestamps: true });
 
 module.exports = mongoose.model('Escalation', escalationSchema);
