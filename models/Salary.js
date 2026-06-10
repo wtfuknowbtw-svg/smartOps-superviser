ab84bbaff2cb3cc056bc260b9dd211070f079bd9
ummyeahumm <wtfuknowbtw@gmail.com>
2026-06-10 16:51:51 +1200

    Update Salary.js

--- models/Salary.js
+++ models/Salary.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const salarySchema = new mongoose.Schema({
-  // Add fields here
+  
 }, { timestamps: true });
 
 module.exports = mongoose.model('Salary', salarySchema);
