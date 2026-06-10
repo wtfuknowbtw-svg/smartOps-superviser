62389fef88c555dd1deac773e1b6d53b8a71b73e
ummyeahumm <wtfuknowbtw@gmail.com>
2026-06-10 16:52:01 +1200

    Update StockTransaction.js

--- models/StockTransaction.js
+++ models/StockTransaction.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const stockTransactionSchema = new mongoose.Schema({
-  // Add fields here
+  
 }, { timestamps: true });
 
 module.exports = mongoose.model('StockTransaction', stockTransactionSchema);
